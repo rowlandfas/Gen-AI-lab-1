d51b2fb0f2d4a0e99a3eabfc89445db7e3b7a717
rowlandfas <ayodele.rowland@cloudhight.com>
2025-11-11 15:12:58 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,4 +13,4 @@ if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
 User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME','$DJANGO_SUPERUSER_EMAIL','$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell || true
 fi
 # run server
-gunicorn chatbot_project.wsgi:application --bind 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
