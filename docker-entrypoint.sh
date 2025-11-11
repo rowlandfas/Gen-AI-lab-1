ebc18843afbc59e2731f1c4bbc6c21c04fd6d2a5
rowlandfas <ayodele.rowland@cloudhight.com>
2025-11-11 15:26:47 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,4 +13,4 @@ if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
 User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME','$DJANGO_SUPERUSER_EMAIL','$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell || true
 fi
 # run server
-python manage.py runserver 0.0.0.0:8000
+exec python manage.py runserver 0.0.0.0:8000
